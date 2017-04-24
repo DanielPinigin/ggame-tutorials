b7108f33f1fe5d09e979a2e78064cc3cf719f2ce
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-04-24 14:36:01 -0400

    Updated from Brython Server: 4/24/2017 2:36:00 PM

--- tutorial4.py
+++ tutorial4.py
@@ -23,7 +23,7 @@ class SpaceGame(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
-        bg = Sprite(bg_asset, (0,0))
+        #bg = Sprite(bg_asset, (0,0))
         SpaceShip((100,100))
 
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
